721b9ebd3a469ff8e405be035a6185ec05a5ad1d
Youngmin Kim <ymkim92@gmail.com>
2023-07-03 21:32:59 +1000

    able to run gostop command

--- src/gostop/__main__.py
+++ src/gostop/__main__.py
@@ -1,41 +1,47 @@
-import sys
-import random
 import argparse
+import random
+import sys
 
 from gostop import GameState, HumanAgent, RandomAgent
 
 
-def main(argv):
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
-    parser.add_argument('-f', '--fixed-random-seed', dest='fixed_random_seed',
-                        help='Use a fixed random seed to play a predictable game')
+    parser.add_argument(
+        "-f",
+        "--fixed-random-seed",
+        dest="fixed_random_seed",
+        help="Use a fixed random seed to play a predictable game",
+    )
 
     args = parser.parse_args()
 
     if args.fixed_random_seed:
         random.seed(args.fixed_random_seed)
 
-    players = [HumanAgent('Human'), RandomAgent('Deep Pink')]
+    players = [HumanAgent("Human"), RandomAgent("Deep Pink")]
     state = GameState.new_game()
 
     while True:
         current_player = players[state.current_player]
-        print('*** {0}'.format(current_player))
+        print("*** {0}".format(current_player))
         print(state)
 
         possible_actions = state.get_possible_actions()
         if len(possible_actions) == 0:
-            raise Exception('No more actions')
+            raise Exception("No more actions")
         action = current_player.get_action(state, possible_actions)
-        print('*** {0} takes action {1}'.format(current_player, str(action)))
+        print("*** {0} takes action {1}".format(current_player, str(action)))
 
         last_player = state.current_player
         state = state.generate_successor(action)
-        if (state.get_result(last_player) == 1):
-            print('*** {0} wins!'.format(players[last_player]))
+        if state.get_result(last_player) == 1:
+            print("*** {0} wins!".format(players[last_player]))
             break
 
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+if __name__ == "__main__":
+    main()
